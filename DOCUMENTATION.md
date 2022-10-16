5d64d7458fac1b691edc5bcd60efed5fed4f3218
jakob.jungreithmeir <jakob.jungreithmeir@vbcf.ac.at>
2022-10-16 12:51:26 +0200

    Add java spring boot app for bpmn deployment

--- DOCUMENTATION.md
+++ DOCUMENTATION.md
@@ -27,6 +27,15 @@ Check whether tables are created properly
 Check REST response from http://localhost:8080/engine-rest/process-instance/count
 
 
+#### Verify WebApp Functionality
+
+All webapps can be accessed through the following link: http://localhost:8080/camunda-welcome/index.html
+
+The cockpit may be reached through http://localhost:8080/camunda/app/cockpit/default/#/dashboard
+
 ## Sources
 - https://github.com/camunda/docker-camunda-bpm-platform/blob/next/test/docker-compose.yml
 - https://docs.camunda.org/manual/latest/reference/rest/process-instance/
+- https://www.youtube.com/watch?v=fLeUaVwpuZw
+- https://stackoverflow.com/questions/68562246/access-remote-camunda-service-programmatically
+- https://github.com/camunda-community-hub/camunda-platform-7-rest-client-java
---
+++ src/main/java/restapp/ApplicationController.java
@@ -0,0 +1,37 @@
+package restapp;
+
+import java.io.File;
+import lombok.AllArgsConstructor;
+import org.camunda.community.rest.client.api.DeploymentApi;
+import org.camunda.community.rest.client.api.ProcessDefinitionApi;
+import org.camunda.community.rest.client.invoker.ApiException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class ApplicationController {
+
+  private DeploymentApi deploymentApi;
+
+  @GetMapping("/")
+  public String index() {
+    return "Greetings from Spring Boot!";
+  }
+
+  @GetMapping("/deploy")
+  public String deploy() throws ApiException {
+    deploymentApi.createDeployment(
+        null,
+        null,
+        true,
+        true,
+        "test_deployment",
+        null,
+        new File(Application.class.getClassLoader().getResource("testdiagram.bpmn").getFile())
+    );
+    return "Deployed successfully";
+  }
+
+
+}
